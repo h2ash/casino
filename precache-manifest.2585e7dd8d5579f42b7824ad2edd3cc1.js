09861a1b67def16a686350ade02c966ca65e03d2
h2ash <hun22ash@gmail.com>
2020-02-17 22:32:35 +0200

    Updates

---
+++ precache-manifest.2585e7dd8d5579f42b7824ad2edd3cc1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de34805f0653560a317cde23ef554a68",
+    "url": "/casino/index.html"
+  },
+  {
+    "revision": "d573ddcff1f8822865bb",
+    "url": "/casino/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0085382bfd862bd0523f",
+    "url": "/casino/static/js/2.3efdfe61.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/casino/static/js/2.3efdfe61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d573ddcff1f8822865bb",
+    "url": "/casino/static/js/main.f93e2790.chunk.js"
+  },
+  {
+    "revision": "c5842f9185f1b4d40c49",
+    "url": "/casino/static/js/runtime-main.05c43aa7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/casino/static/media/logo.5d5d9eef.svg"
+  }
+]);
