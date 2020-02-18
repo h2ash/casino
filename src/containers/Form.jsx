618a70d033d46487b687a76168c246064f475849
h2ash <hun22ash@gmail.com>
2020-02-18 18:26:48 +0200

    for Dhubi

--- src/containers/Form.jsx
+++ src/containers/Form.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import logo from '../images/logo.svg';
 
 const Form = () => (
-  <div className="form"></div>
+  <div className="form">
+    <img className="form__logo" src={logo} alt="logo" />
+    <h1 className="form__heading">م	حباً، يس	ا لقاؤك</h1>
+    <span className="form__advice"> ﻣﻦ ﺧﺒﺮاﺋﻨﺎ؟ اﺗﺮك ﺗﻔﺎﺻﻴﻠﻚ ﻫﻨﺎ، وﺳﻮف ﻳﻘﻮم ﻣﻨﺪوﺑﻨﺎ ﺑﺎﻟﺮد ﻋﻠﻚﻴ
+ﻫﻞ ﺗﺮﻳﺪ ﻧﺼﺎﺋﺢ ﻣﻔﻴﺪة
+    </span>
+  </div>
 );
 
 export default Form;
